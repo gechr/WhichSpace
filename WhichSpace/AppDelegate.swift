25e1341863b66ad7599015d337e3805972714321
George Christou <gechr@users.noreply.github.com>
2015-10-28 09:59:26 +0000

    Fix whitespace (damn Xcode!) + correct app name

--- WhichSpace/AppDelegate.swift
+++ WhichSpace/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  Spacey
+//  WhichSpace
 //
 //  Created by George on 27/10/2015.
 //  Copyright © 2015 George Christou. All rights reserved.
@@ -10,13 +10,13 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
+
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var statusMenu: NSMenu!
-    
+
     var icons = [NSImage]()
     let statusBarItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
-    
+
     func applicationWillFinishLaunching(notification: NSNotification) {
         NSApp.setActivationPolicy(.Prohibited)
         NSWorkspace.sharedWorkspace().notificationCenter.addObserver(
@@ -26,7 +26,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             object: NSWorkspace.sharedWorkspace()
         )
     }
-    
+
     func applicationDidFinishLaunching(notification: NSNotification) {
         for i in 0...5 {
             let iconName = "ic_looks_" + String(i + 1)
@@ -36,13 +36,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusBarItem.image = icons[0]
         statusBarItem.menu = statusMenu
     }
-    
+
     func activeSpaceDidChange() {
         // TODO: Figure out how to determine the *actual* space number
         let spaceNumber = Int(arc4random_uniform(UInt32(icons.count)))
         statusBarItem.image = icons[spaceNumber]
     }
-    
+
     @IBAction func quitClicked(sender: NSMenuItem) {
         NSApplication.sharedApplication().terminate(self)
     }
