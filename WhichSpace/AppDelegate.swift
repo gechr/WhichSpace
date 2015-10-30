afcd797467c37001195bf48e8627e3611c0d47f3
Stephen Sykes <sdsykes@gmail.com>
2015-10-30 11:01:49 +0200

    Draw menu item instead of using images allowing any number of spaces

--- WhichSpace/AppDelegate.swift
+++ WhichSpace/AppDelegate.swift
@@ -9,13 +9,14 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var statusMenu: NSMenu!
 
     var icons = [NSImage]()
-    let statusBarItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    let statusBarItem = NSStatusBar.systemStatusBar().statusItemWithLength(27)
+    let conn = _CGSDefaultConnection()
 
     func applicationWillFinishLaunching(notification: NSNotification) {
         NSApp.setActivationPolicy(.Prohibited)
@@ -25,40 +26,45 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             name: NSWorkspaceActiveSpaceDidChangeNotification,
             object: NSWorkspace.sharedWorkspace()
         )
+        
+        statusBarItem.button?.cell = StatusItemCell()
     }
 
     func applicationDidFinishLaunching(notification: NSNotification) {
-        icons.append(NSImage(named: "Default")!)
-        icons[0].template = true
-        for i in 1...6 {
-            let iconName = "space_" + String(i)
-            icons.append(NSImage(named: iconName)!)
-            icons[i].template = true
-        }
-        statusBarItem.image = icons.first
+        statusBarItem.image = NSImage(named: "Default")
         statusBarItem.menu = statusMenu
+        
+        // show the correct space on launch
+        activeSpaceDidChange()
     }
 
     func activeSpaceDidChange() {
-        let conn = _CGSDefaultConnection()
         let info = CGSCopyManagedDisplaySpaces(conn)
         let displayInfo = info[0] as! NSDictionary
         let activeSpaceID = displayInfo["Current Space"]!["ManagedSpaceID"] as! Int
         let spaces = displayInfo["Spaces"] as! NSArray
         for (index, space) in spaces.enumerate() {
             let spaceID = space["ManagedSpaceID"] as! Int
-            let iconIndex = index + 1
+            let spaceNumber = index + 1
             if spaceID == activeSpaceID {
-                if iconIndex >= icons.count {
-                    break
-                }
-                statusBarItem.image = icons[iconIndex]
+                statusBarItem.button?.title = String(spaceNumber)
                 return
             }
         }
-        statusBarItem.image = icons.first
     }
 
+    func menuWillOpen(menu: NSMenu) {
+        if let cell = statusBarItem.button?.cell as! StatusItemCell? {
+            cell.isMenuVisible = true
+        }
+    }
+    
+    func menuDidClose(menu: NSMenu) {
+        if let cell = statusBarItem.button?.cell as! StatusItemCell? {
+            cell.isMenuVisible = false
+        }
+    }
+    
     @IBAction func quitClicked(sender: NSMenuItem) {
         NSApplication.sharedApplication().terminate(self)
     }
