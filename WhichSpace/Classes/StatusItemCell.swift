6a6a2a0ed038f86c39737000e0b794ad8e7bac91
George Christou <gechrr@gmail.com>
2015-11-18 20:47:11 +0000

    Tweak menu bar icon colours

--- WhichSpace/Classes/StatusItemCell.swift
+++ WhichSpace/Classes/StatusItemCell.swift
@@ -13,21 +13,25 @@ class StatusItemCell: NSStatusBarButtonCell {
     var isMenuVisible = false
     
     override func drawImage(image: NSImage, withFrame frame: NSRect, inView controlView: NSView) {
-        let darkColor = NSColor(calibratedWhite: 0, alpha: 1)
-        let whiteColor = NSColor(calibratedWhite: 1, alpha: 1)
-        let blueColor = NSColor(calibratedRed: 0, green: 0.41, blue: 0.85, alpha: 1)
 
-        var foregroundColor = isMenuVisible ? darkColor : whiteColor
-        var backgroundColor = isMenuVisible ? whiteColor : darkColor
+        var darkColor: NSColor
+        var whiteColor: NSColor
+        if AppDelegate.darkModeEnabled {
+            darkColor = NSColor(calibratedWhite: 0.7, alpha: 1)
+            whiteColor = NSColor(calibratedWhite: 0, alpha: 1)
+        } else {
+            darkColor = NSColor(calibratedWhite: 0.3, alpha: 1)
+            whiteColor = NSColor(calibratedWhite: 1, alpha: 1)
+        }
+
+        let blueColor = NSColor(calibratedRed: 0, green: 0.41, blue: 0.85, alpha: 1)
+        let foregroundColor = isMenuVisible ? darkColor : whiteColor
+        let backgroundColor = isMenuVisible ? whiteColor : darkColor
         
         if isMenuVisible {
             let rectPath = NSBezierPath(rect: frame)
             blueColor.setFill()
             rectPath.fill()
-        }
-
-        if AppDelegate.darkModeEnabled {
-            swap(&backgroundColor, &foregroundColor)
         }
 
         let roundedRectanglePath = NSBezierPath(roundedRect: NSRect(x: 5, y: 3, width: 16, height: 16), xRadius: 2, yRadius: 2)
